aaed37e08b7c5bb4950ce8045c5e5c5c7876c0f2
i0x60d5 <i0x60d5@outlook.com>
2022-06-13 09:09:42 -0400

    Top Interview Questions - Modified Single Number Solution

--- Top Interview Questions (C++)/Single Number/solution.cpp
+++ Top Interview Questions (C++)/Single Number/solution.cpp
@@ -22,8 +22,12 @@ int main(int argc, char** argv){
   std::vector<int> v1 = {2,2,1};
   std::vector<int> v2 = {4,1,2,1,2};
   std::vector<int> v3 = {1};
+  std::vector<int> v4 = {3,5,5,66,6,6,7,7,8,8,9,9};
+  std::vector<int> v5 = {5, 5, 7, 7, 8, 8, 9, 9 ,1, 0};
   std::cout << singleNumber(v1) << std::endl;
   std::cout << singleNumber(v2) << std::endl;
   std::cout << singleNumber(v3) << std::endl;
+  std::cout << singleNumber(v4) << std::endl;
+  std::cout << singleNumber(v5) << std::endl;
   return 0;
 }
